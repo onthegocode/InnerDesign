0ae3343a1f40b0aaa291d2a96553e59c41f49e01
Andrew Rivera <34843760+onthegocode@users.noreply.github.com>
2022-04-03 14:34:50 -0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -50,3 +50,92 @@ function scrollProgress() {
 }
 
 document.addEventListener("scroll", scrollProgress);
+
+// /////////////////////////////////
+// Animate on View
+///////////////////////////////////
+
+(function () {
+	var elements;
+	var elements2;
+	var elements3;
+	var elements4;
+	var elements5;
+	var windowHeight;
+
+	function init() {
+		elements = document.querySelectorAll(".hidden");
+		elements2 = document.querySelectorAll(".hidden2");
+		elements3 = document.querySelectorAll(".hidden3");
+		elements4 = document.querySelectorAll(".hidden4");
+		elements5 = document.querySelectorAll(".hidden5");
+		windowHeight = window.innerHeight;
+	}
+
+	function checkPosition() {
+		//interior
+		for (var i = 0; i < elements.length; i++) {
+			var element = elements[i];
+			var positionFromTop = elements[i].getBoundingClientRect().top;
+
+			if (positionFromTop - windowHeight / 2 <= 0) {
+				element.classList.add("interior__animate");
+				element.classList.remove("hidden");
+			}
+		}
+		//styles
+		for (var i = 0; i < elements2.length; i++) {
+			var element = elements2[i];
+			var positionFromTop = elements2[i].getBoundingClientRect().top;
+
+			if (positionFromTop - windowHeight / 2 <= 0) {
+				element.classList.add("styles__animate");
+				element.classList.remove("hidden2");
+			}
+		}
+		//dream
+		for (var i = 0; i < elements3.length; i++) {
+			var element = elements3[i];
+			var positionFromTop = elements3[i].getBoundingClientRect().top;
+
+			if (positionFromTop - windowHeight / 2 <= 0) {
+				element.classList.add("dream__animate");
+				element.classList.remove("hidden3");
+			}
+		}
+		//footer
+		for (var i = 0; i < elements4.length; i++) {
+			var element = elements4[i];
+			var positionFromTop = elements4[i].getBoundingClientRect().top;
+
+			if (positionFromTop - windowHeight / 1.5 <= 0) {
+				element.classList.add("footer__animate");
+				element.classList.remove("hidden4");
+			}
+		}
+
+		for (var i = 0; i < elements5.length; i++) {
+			var element = elements5[i];
+			var positionFromTop = elements5[i].getBoundingClientRect().bottom;
+
+			if (positionFromTop - windowHeight * 1.2 <= 0) {
+				element.classList.add("copyright__animate");
+				element.classList.remove("hidden5");
+			}
+		}
+	}
+
+	window.addEventListener("scroll", checkPosition);
+	window.addEventListener("resize", init);
+
+	init();
+	checkPosition();
+})();
+
+document.onscroll = function () {
+	if ((window.innerHeight + window.scrollY) / 2 > document.body.clientHeight) {
+		document.getElementById("scroll-top").style.display = "block";
+	} else {
+		document.getElementById("scroll-top").style.display = "none";
+	}
+};
